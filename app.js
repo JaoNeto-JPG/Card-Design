068fe580050366a9c06f0aab420afe61cfce1173
João Neto <joaoalvessiqueira18@gmail.com>
2025-11-09 15:12:13 -0300

    Atualização na sombra no card

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+const form = document.getElementById("formCadastros");
+const nome = document.getElementById("name");
+
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+});
